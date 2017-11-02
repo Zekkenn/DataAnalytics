244234fa2c60f099ba1b0449c74711cbb1bd5477
Zekkenn <sebastian.moreno-r@mail.escuelaing.edu.co>
2017-11-02 04:28:03 -0500

    anomalies

--- src/main/resources/static/js/game/evaluarMateria.js
+++ src/main/resources/static/js/game/evaluarMateria.js
@@ -10,17 +10,32 @@ var evaluarMateria = (function () {
         var decision = "";
         if(result.decision === 'Cancele'){
             tittle = "JUMM.. ESTA COMPLICADO";
-            image = "../images/mal.png";
+            image = "/images/mal.png";
             decision = "Lo mejor que puedes hacer es Cancelar";
         }else{
             tittle = "LO ESTAS HACIENDO BIEN";
-            image = "../images/bien.png";
+            image = "/images/bien.png";
             decision = "No te descuides";
         }
         document.getElementById("status").src = image;
         document.getElementById("tercio3").innerHTML = tittle;
         document.getElementById("rSquared").innerHTML = content;
         document.getElementById("decision").innerHTML = decision;
+    };
+    
+    var plot = function (id, src){
+        $(id).html(
+            '<iframe id="Plot1"'
+            +        'style="text-align: center;"'
+            +    'name="Pimb"'
+            +    'title="Plot"'
+            +    'width="800"'
+            +    'height="500"'
+            +    'frameborder="5"'
+            +    'scrolling="no"'
+            +    'src="/html/' + src + '">'
+            +'</iframe>'
+        );
     };
     
     return {
@@ -60,25 +75,37 @@ var evaluarMateria = (function () {
                 }
             });
         },
-        addPlot : function (){
+        addPlotBySubjects : function (){
             var src = '';
             if ($("#materia").val() === "PIMB") {
                 src = "plotPimb3dTercios.html";
+                plot('#plot1', src);
             } else if ($("#materia").val() === "MBDA"){
                 src = "finalVspromMbda.html";
+                plot('#plot1', src);
             }
-            $('#plot1').html(
-                    '<iframe id="Plot1"'
-                    +        'style="text-align: center;"'
-                    +    'name="Pimb"'
-                    +    'title="Plot"'
-                    +    'width="800"'
-                    +    'height="500"'
-                    +    'frameborder="5"'
-                    +    'scrolling="no"'
-                    +    'src="/html/' + src + '">'
-                    +'</iframe>'
-                    );
+        },
+        addPlotByAnomalies : function (){
+            var src = '';
+            if ($("#department").val() === "Sistemas") {
+                src = "Anom_notasVsSemsSistemas.html";
+                plot('#plot1', src);
+            } else if ($("#department").val() === "Matematicas"){
+                src = "Anom_notasVsSemsMatematicas.html";
+                plot('#plot1', src);
+            } else if ($("#department").val() === "Humanidades"){
+                src = "Anom_notasVsSemsHumanidades.html";
+                plot('#plot1', src);
+            } else if ($("#department").val() === "Industrial"){
+                src = "Anom_notasVsSemsIndustrial.html";
+                plot('#plot1', src);
+            } else if ($("#department").val() === "Electronica"){
+                src = "Anom_notasVsSemsElectronica.html";
+                plot('#plot1', src);
+            } else if ($("#department").val() === "Electrica"){
+                src = "Anom_notasVsSemsElectrica.html";
+                plot('#plot1', src);
+            }
         }
 
     };
